6c6d325e838b27d875abd85246a4e19157ad6a80
aseelflihan <aseelflihan@gmail.com>
2023-03-21 13:03:48 +0300

    complate assignmen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route,Switch } from "react-router-dom/cjs/react-router-dom.min";
+
 
 function Home() {
   return (
@@ -8,9 +10,47 @@ function Home() {
     </div>
   );
 }
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
 
 function App() {
-  return <Home />;
+  return(
+    <BrowserRouter>
+   <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
